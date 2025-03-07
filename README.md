2a6c612272c2361f35f70f1b2b7e8ef4a15e3c78
Adrianhmmr <adrian10hammer@gmail.com>
2025-03-07 14:12:55 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # DailyHoroscope
+
+Learning Twilio API, virtual env, and Python
+
+Requirements:
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate it (Mac/Linux)
+source venv/bin/activate
+
+# Or on Windows
+venv\Scripts\activate
+
+# Install flask
+bin/pip3 install -r requirements.txt
+
+
+
+
+
+
---
+++ send.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+account_sid = TWILIO_ACCOUNT_SID
+auth_token = TWILIO_AUTH_TOKEN
+
+client = Client(account_sid, auth_token)
+
+from_whatsapp_number = "whatsapp:+14155238886"
+to_whatsapp_number = "whatsapp:+4791166190"
+
+client.messages.create(body="Ahoy Galaxy!",
+                       from_=from_whatsapp_number,
+                       to=to_whatsapp_number)
